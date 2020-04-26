cb85cd4fb0bbf258b6185770e08ebca94a5cfa38
Vivek Raman <vr.ac4bf@live.com>
2020-04-27 02:16:21 +0530

    Added settings categories and tab switching.

---
+++ Assets/Scripts/Main Menu/Settings/SettingsTabSwitcher.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsTabSwitcher : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> rects = null;
+
+    public void SwitchTo(int selection)
+    {
+        for (int i = 0; i < rects.Count; i++)
+        {
+            rects[i].SetActive(i == selection);
+        }
+    }
+}
