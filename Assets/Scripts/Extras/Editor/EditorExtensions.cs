96b5150d7cf88a9631917d00a93ee374a21f3bf6
Vivek Raman <vr.ac4bf@live.com>
2020-06-06 10:14:37 +0530

    Updated with more Editor Extensions

--- Assets/Scripts/Extras/Editor/EditorExtensions.cs
+++ Assets/Scripts/Extras/Editor/EditorExtensions.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 using System.Linq;
 using Object = System.Object;
 
-// credit: https://forum.unity.com/threads/shortcut-key-for-lock-inspector.95815/
-public class ToggleInspectorLockWithAltQ: Editor
+public class EditorExtensions : Editor
 {
+
+    #region Lock Inspector
+    // credit: https://forum.unity.com/threads/shortcut-key-for-lock-inspector.95815/
+
     private static EditorWindow _mouseOverWindow;
  
     [MenuItem("Extensions/Select Inspector under cursor #&q")]
-    static void SelectLockableInspector()
+    private static void SelectLockableInspector()
     {
         if (EditorWindow.mouseOverWindow.GetType().Name == "InspectorWindow")
         {
@@ -24,7 +27,7 @@ public class ToggleInspectorLockWithAltQ: Editor
     }
  
     [MenuItem("Extensions/Toggle Inspector Lock &q")]
-    static void ToggleInspectorLock()
+    private static void ToggleInspectorLock()
     {
         if (_mouseOverWindow == null)
         {
@@ -46,5 +49,50 @@ public class ToggleInspectorLockWithAltQ: Editor
             _mouseOverWindow.Repaint();
         }
     }
+    
+    #endregion
 
+    #region Clear Console
+    // credit: https://answers.unity.com/questions/707636/clear-console-window.html
+    
+    [MenuItem ("Extensions/Clear Console #&c")]
+    private static void Clear ()
+    {
+        var logEntries = System.Type.GetType("UnityEditor.LogEntries,UnityEditor.dll");
+        var clearMethod = logEntries.GetMethod(
+            "Clear", 
+            System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
+        clearMethod.Invoke(null,null);
+    }
+    
+    #endregion
+
+    #region Snap to Ground
+    // credit: https://unity3d.college/2017/09/26/using-unity-editor-extensions-to-snap-to-ground-when-placing-gameobjects/
+    
+    [MenuItem("Extensions/Snap To Ground &g")]
+    public static void Ground()
+    {
+        foreach(var transform in Selection.transforms)
+        {
+            var hits = Physics.RaycastAll(transform.position + Vector3.up, Vector3.down, 10f);
+            foreach(var hit in hits)
+            {
+                if (hit.collider.gameObject == transform.gameObject)
+                    continue;
+
+                transform.position = hit.point;
+                break;
+            }
+        }
+    }
+    
+    #endregion
+    
+    #region Template
+    // credit: 
+    
+
+    #endregion
+    
 }
