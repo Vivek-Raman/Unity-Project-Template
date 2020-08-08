2d0b66f60d49509ca33b053c586b403143cb35a8
Vivek Raman <vr.ac4bf@live.com>
2020-08-08 09:40:25 +0530

    Added state machine

---
+++ Assets/Scripts/Extras/State Machine/State.cs
@@ -0,0 +1,13 @@
+﻿public abstract class State
+{
+    protected StateMachine source = null;
+
+    public virtual void OnStateEnter() { }
+    public virtual void OnStateTick()  { }
+    public virtual void OnStateExit()  { }
+
+    public State(StateMachine source)
+    {
+        this.source = source;
+    }
+}
---
+++ Assets/Scripts/Extras/State Machine/StateMachine.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+    public State currentState = null;
+    public State initialState = null;
+
+    protected void Start()
+    {
+        currentState = initialState;
+    }
+
+    protected void Update()
+    {
+        currentState?.OnStateTick();
+    }
+
+    public void SetState(State toSet)
+    {
+        currentState?.OnStateExit();
+        currentState = toSet;
+        currentState.OnStateEnter();
+    }
+}
